9e721c73c0f611ab618651018fad017e9cd20d8e
Amit <samalamit.dev@gmail.com>
2026-02-19 23:39:24 +0530

    AVDEV-6 Designed Banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Button, Typography } from '@mui/material'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import LoginPage from './components/LoginPage'
 import RegisterPage from './components/RegisterPage'
 import Profile from './components/profile/Profile'
-import Navbar from './components/Navbar/Navbar'
 import ProductListPage from './components/product/ProductListPage'
+import Home from './components/Home'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login"/>} />
-      <Route path="/navbar" element={<Navbar/>} />
+      <Route path="/home" element={<Home/>} />
       <Route path="/login" element={<LoginPage/>} />
       <Route path="/register" element={<RegisterPage/>} />
       <Route path="/profile" element={<Profile/>} />
