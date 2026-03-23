9166d4ce54fad5db1d5a0f9f1883cc5c1e42126f
Sanjeeb555 <sanjeeb4223@gmail.com>
2026-03-24 01:54:46 +0530

    Logic for Filter Component

--- src/components/product/ProductsList.jsx
+++ src/components/product/ProductsList.jsx
@@ -5,8 +5,10 @@ const productsData = [
   {
     id: 1,
     name: "Apple iPhone 17 (Sage, 256 GB)",
+    brand: "Apple",
+    category: "Mobiles",
     image: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/n/v/e/-original-imahft5gfchxyewy.jpeg?q=70",
-    price: "₹82,900",
+    price: 82900,
     exchangeOffer: "Upto ₹54,100 Off on Exchange",
     bankOffer: "Bank Offer Available",
     specs: [
@@ -20,8 +22,10 @@ const productsData = [
   {
     id: 2,
     name: "Samsung Galaxy S24 (Black, 256 GB)",
+    brand: "Samsung",
+    category: "Mobiles",
     image: "https://www.designinfo.in/wp-content/uploads/2024/01/Samsung-Galaxy-S24-128GB-Onyx-Black-1-485x485.webp",
-    price: "₹74,999",
+    price: 74999,
     exchangeOffer: "Upto ₹40,000 Off on Exchange",
     bankOffer: "Bank Offer Available",
     specs: [
@@ -35,8 +39,10 @@ const productsData = [
   {
     id: 3,
     name: "OnePlus 12 (Flowy Emerald, 256 GB)",
+    brand: "Samsung",
+    category: "Mobiles",
     image: "https://m.media-amazon.com/images/I/717Qo4MH97L._AC_UF1000,1000_QL80_.jpg",
-    price: "₹64,999",
+    price: 64999,
     exchangeOffer: "Upto ₹35,000 Off on Exchange",
     bankOffer: "Bank Offer Available",
     specs: [
@@ -47,12 +53,40 @@ const productsData = [
       "1 Year Warranty",
     ],
   },
+  
 ];
 
-const ProductsList = ({ searchQuery }) => {
+const ProductsList = ({ searchQuery, filter }) => {
+  console.log(filter);
 
   const filteredProducts = productsData.filter((product) => {
-    return product.name.toLowerCase().includes(searchQuery.toLowerCase());
+    let search = product.name.toLowerCase().includes(searchQuery.toLowerCase());
+    
+    let category,brand,price;
+
+    if (filter.category.length > 0) {
+      category = filter.category.includes(product.category);
+    } else {
+      category = true;
+    }
+    
+    if (filter.brand.length > 0) {
+      brand = filter.brand.includes(product.brand);
+    } else {
+      brand = true;
+    }
+    
+    if (filter.price.length > 0) {
+      if (product.price >= filter.price[0] && product.price <= filter.price[1]) {
+        price = true;
+      } else {
+        price = false;
+      }
+    } else {
+      price = true;
+    }
+
+    return search && category && brand && price;
   })
 
   console.log(filteredProducts);
